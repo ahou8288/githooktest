1a670a57bc4aad3eefac31e92ac1eca5ee9df9ad
Andrew Houghton <houghtonandrew0@gmail.com>
2018-09-19 10:43:02 +1000

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import bs4
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -9,3 +10,4 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+    logger.info("Bs4 version " + bs4.__version__)
